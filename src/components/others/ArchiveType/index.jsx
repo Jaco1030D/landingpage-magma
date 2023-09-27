57aaf1d94f5157a7a8b1f9bb64aae198b32a6052
Jaco Antonio <jaco42818@gmail.com>
2023-09-27 00:00:05 -0300

    Finalizacao do formulario

--- src/components/others/ArchiveType/index.jsx
+++ src/components/others/ArchiveType/index.jsx
@@ -3,14 +3,19 @@ import { languages } from '../../../constants'
 import { MultiSelect } from 'react-multi-select-component';
 
 const ArchiveType = ({data, updateFieldHandler, inputs}) => {
-   const [selectedValue, setSelectedValue] = useState('');
     const [selected, setSelected] = useState([]);
+    const [selectedOrigin, setSelectedOrigin] = useState([]);
 
-   const handleSelectChange = (event) => {
-     const selectedOption = event.target.value;
-     
-     setSelectedValue(selectedOption);
-   };
+    const handleChange = async (option) => {
+      console.log(option);
+      if (selectedOrigin[0]) {
+        console.log(selectedOrigin);
+        setSelectedOrigin([option[1]])
+      } else {
+        setSelectedOrigin(option)
+      }
+    }
+    
   return (
     <div className='input-steps-content'>
         <div className="input">
@@ -20,12 +25,20 @@ const ArchiveType = ({data, updateFieldHandler, inputs}) => {
         </div>
         <div className="input">
             <label htmlFor="">{inputs[1]}</label> <br />
-            <select value={selectedValue} name="origem" onChange={handleSelectChange} id="">
+            <MultiSelect
+              options={languages}
+              value={selectedOrigin}
+              onChange={handleChange}
+              labelledBy="Select"
+              hasSelectAll={false}
+              closeOnChangedValue={true}
+            />
+            {/* <select value={data.origin} name="origem" onChange={(e) => updateFieldHandler("origin", e.target.value)} id="">
               {languages.map((item, index) => (
                 <option value={item.value}>{item.label}</option>
               ))}
 
-            </select>
+            </select> */}
             {/* <input type="text" name='origem' value={data.origin || ""} onChange={(e) => updateFieldHandler("origin", e.target.value)} placeholder='Qual é o idioma do documento?' required/> */}
         </div>
         <div className="input">
@@ -36,6 +49,7 @@ const ArchiveType = ({data, updateFieldHandler, inputs}) => {
               value={selected}
               onChange={setSelected}
               labelledBy="Select"
+              hasSelectAll={false}
             />
             {/* <select value={""} name="tradução" id="traducao">
               {languages.map((item, index) => (
