f2d7888a01efec880f9a441e1dcf221cbb751a43
Jaco Antonio <jaco42818@gmail.com>
2023-09-28 08:27:02 -0300

    Criação da pagina obrigado

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Footer, Navbar } from '../../components';
 import Main from '../Main';
 import Thanks from '../Thanks';
