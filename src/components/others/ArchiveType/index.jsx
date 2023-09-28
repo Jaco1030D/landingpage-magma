9f82829257f6815d8ba14b04f042e778114fb72d
Jaco Antonio <jaco42818@gmail.com>
2023-09-27 22:22:37 -0300

    Criação da pagina obrigado

--- src/components/others/ArchiveType/index.jsx
+++ src/components/others/ArchiveType/index.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react'
-import { languages, typeContent } from '../../../constants'
+import { languages} from '../../../constants'
 import { MultiSelect } from 'react-multi-select-component';
 
 const ArchiveType = ({data, updateFieldHandler, inputs}) => {
     const [selected, setSelected] = useState([]);
-    const [selectedOrigin, setSelectedOrigin] = useState([]);
-    const [selectedArchiveType, setSelectedArchiveType] = useState([])
+    // const [selectedOrigin, setSelectedOrigin] = useState([]);
+    // const [selectedArchiveType, setSelectedArchiveType] = useState([])
 
-    const handleChangeOrigin = (option) => {
-      console.log(option);
-      if (selectedOrigin[0]) {
-        setSelectedOrigin(prev => {
-          if (option[1]) {
-            updateFieldHandler("origin", option[1].label)
-            return [option[1]]  
-          } else {
-            updateFieldHandler("origin", [])
-            return []
-          }
-        })
-      } else {
-        setSelectedOrigin(prev => {
-          updateFieldHandler("origin", option[0].label || "")
-          return option
-        })
-      }
-    }
+    // const handleChangeOrigin = (option) => {
+    //   console.log(option);
+    //   if (selectedOrigin[0]) {
+    //     setSelectedOrigin(prev => {
+    //       if (option[1]) {
+    //         updateFieldHandler("origin", option[1].label)
+    //         return [option[1]]  
+    //       } else {
+    //         updateFieldHandler("origin", [])
+    //         return []
+    //       }
+    //     })
+    //   } else {
+    //     setSelectedOrigin(prev => {
+    //       updateFieldHandler("origin", option[0].label || "")
+    //       return option
+    //     })
+    //   }
+    // }
     const handleChangeTranslation = (options) => {
       let string = []
       options.forEach(element => {
@@ -36,50 +36,49 @@ const ArchiveType = ({data, updateFieldHandler, inputs}) => {
         return options
       })
     }
-    const handleChangeContent = (option) => {
-      console.log(option);
-      if (selectedArchiveType[0]) {
-        setSelectedArchiveType(prev => {
-          if (option[1]) {
-            updateFieldHandler("typeArchive", option[1].label)
-            return [option[1]]  
-          } else {
-            updateFieldHandler("typeArchive", [])
-            return []
-          }
-        })
-      } else {
-        setSelectedArchiveType(prev => {
-          updateFieldHandler("typeArchive", option[0].label || "")
-          return option
-        })
-      }
-    }
+    // const handleChangeContent = (option) => {
+    //   console.log(option);
+    //   if (selectedArchiveType[0]) {
+    //     setSelectedArchiveType(prev => {
+    //       if (option[1]) {
+    //         updateFieldHandler("typeArchive", option[1].label)
+    //         return [option[1]]  
+    //       } else {
+    //         updateFieldHandler("typeArchive", [])
+    //         return []
+    //       }
+    //     })
+    //   } else {
+    //     setSelectedArchiveType(prev => {
+    //       updateFieldHandler("typeArchive", option[0].label || "")
+    //       return option
+    //     })
+    //   }
+    // }
     
   return (
     <div className='input-steps-content'>
         <div className="input">
             <label htmlFor="">{inputs[0]}</label> <br />
-            <MultiSelect
+            {/* <MultiSelect
               options={typeContent}
               value={selectedArchiveType}
               onChange={handleChangeContent}
               hasSelectAll={false}
               closeOnChangedValue={true}
-              aria-readonly={false}
-            />
+            /> */}
             
             {/* <input type="text" name='conteudo' value={data.typeArchive || ""} onChange={(e) => updateFieldHandler("typeArchive", e.target.value)}  placeholder='Tipo de conteudo: pdf, img ...' required/> */}
         </div>
         <div className="input">
             <label htmlFor="">{inputs[1]}</label> <br />
-            <MultiSelect
+            {/* <MultiSelect
               options={languages}
               value={selectedOrigin}
               onChange={handleChangeOrigin}
               hasSelectAll={false}
               closeOnChangedValue={true}
-            />
+            /> */}
             {/* <select value={data.origin} name="origem" onChange={(e) => updateFieldHandler("origin", e.target.value)} id="">
               {languages.map((item, index) => (
                 <option value={item.value}>{item.label}</option>
